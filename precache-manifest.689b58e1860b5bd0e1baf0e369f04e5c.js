d1f96b34b7517226510e7b89bd7a2c52d55a0e08
Céline Perriard <perriard.1004.student@ifosupwavre.be>
2020-10-14 21:31:05 +0200

    Updates

---
+++ precache-manifest.689b58e1860b5bd0e1baf0e369f04e5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c53cd8a00e809fa6a24b7002c87b19fa",
+    "url": "/monstre/index.html"
+  },
+  {
+    "revision": "26dd842b48025189c0bf",
+    "url": "/monstre/static/css/main.61601852.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monstre/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstre/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26dd842b48025189c0bf",
+    "url": "/monstre/static/js/main.4a854db0.chunk.js"
+  },
+  {
+    "revision": "fde1c8ca92accb4b46e0",
+    "url": "/monstre/static/js/runtime-main.c4fe69b4.js"
+  }
+]);
